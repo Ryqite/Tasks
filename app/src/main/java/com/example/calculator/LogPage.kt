d398b156d6618a51c41446d5f1ebcb07b43ab7a0
Ryqite <garbaruk.901@gmail.com>
2025-06-09 02:59:00 +0300

    reworked with documentation

--- app/src/main/java/com/example/calculator/LogPage.kt
+++ app/src/main/java/com/example/calculator/LogPage.kt
@@ -1,21 +1,20 @@
 package com.example.calculator
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.calculator.databinding.LogPageBinding
 
 
 class LogPage : AppCompatActivity() {
-    lateinit var binding: LogPageBindingф
+    lateinit var binding: LogPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LogPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val operations=intent.getSerializableExtra("Operations") as List<LogData>
+        val operations = intent.getSerializableExtra("Operations") as List<LogData>
         binding.rvLog.layoutManager = LinearLayoutManager(this)
-        binding.rvLog.adapter= LogAdapter(operations)
+        binding.rvLog.adapter = LogAdapter(operations)
     }
 }
