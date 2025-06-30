899893a4450a9def0c86994fc9a88ccefb6ee007
Ryqite <garbaruk.901@gmail.com>
2025-06-30 14:46:55 +0300

    all tests passed

--- app/src/androidTest/java/com/example/week5/UITests.kt
+++ app/src/androidTest/java/com/example/week5/UITests.kt
@@ -1,12 +1,10 @@
 package com.example.week5
 
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
@@ -18,18 +16,27 @@ import androidx.navigation.navArgument
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.week5.ui.theme.Week5Theme
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class TestNavigationBetweenScreens {
+class UITests {
     @get:Rule
     val rule = createComposeRule()
     private val sampleProducts = mutableListOf(
-        Product(1, "Тестовый продукт", "", "Описание"),
-        Product(2, "Товар 2", "", "Описание 2")
+        Product(
+            1,
+            "Тестовый продукт",
+            "https://www.samsungstore.ru/_next/image/?url=https%3A%2F%2Fcdn.samsungstore.ru%2Fiblock%2Ff89%2Ff89005113fc4a94e95ec29e6569aa810%2Fba0adfa23f3c27e93226fe55c4ce03d9.jpg&w=1080&q=75",
+            "Описание"
+        ),
+        Product(
+            2,
+            "Товар 2",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVzIdiL-k95-9u4D2g17fkzer1NWzo-fVj6w&s",
+            "Описание 2"
+        )
     )
     val product = Product(1, "Тестовый продукт", "", "Описание")
 
@@ -43,11 +50,11 @@ class TestNavigationBetweenScreens {
         rule.onNodeWithText("Ryqite").assertIsDisplayed()
         rule.onNodeWithText("Гарбарук Даниил Александрович").assertIsDisplayed()
         rule.onNodeWithContentDescription("Аватар").assertIsDisplayed()
-        rule.onNodeWithText("Продукты").assertIsDisplayed()
+        rule.onNodeWithContentDescription("НазадИзПрофиля").assertIsDisplayed()
     }
 
     @Test
-    fun testProductsScreen(){
+    fun testProductsScreen() {
         rule.setContent {
             Week5Theme {
                 ProductsScreen(
@@ -67,7 +74,7 @@ class TestNavigationBetweenScreens {
     }
 
     @Test
-    fun testDetailScreen(){
+    fun testDetailScreen() {
         rule.setContent {
             Week5Theme {
                 DetailScreen(
@@ -83,8 +90,9 @@ class TestNavigationBetweenScreens {
         rule.onNodeWithContentDescription("НазадИзДеталей").assertIsDisplayed()
 
     }
+
     @Test
-    fun testBasketScreen(){
+    fun testBasketScreen() {
         val products = mutableStateListOf<Product>()
             .apply { addAll(sampleProducts) }
         var deletedId = 0
@@ -92,7 +100,7 @@ class TestNavigationBetweenScreens {
             Week5Theme {
                 BasketScreen(
                     productsForBuy = products,
-                    deleteFromBasket = {deletedId = it},
+                    deleteFromBasket = { deletedId = it },
                     backIcon = {},
                     navigateToProductPage = {},
                     navigateToBascketPage = {})
@@ -100,20 +108,22 @@ class TestNavigationBetweenScreens {
         }
         rule.onNodeWithText("Корзина").assertIsDisplayed()
         rule.onNodeWithContentDescription("НазадИзКорзины").assertIsDisplayed()
-        rule.onNodeWithContentDescription("УдалитьКнопка").performClick()
-        assertEquals(1,deletedId)
+        rule.onAllNodesWithContentDescription("УдалитьКнопка")[0].performClick()
+        assertEquals(1, deletedId)
     }
+
     @Test
     fun testNavigationBetweenScreens() {
         rule.setContent {
             Week5Theme {
-                val productsForNav = remember { mutableStateListOf<Product>().apply { addAll(testProducts) } }
+                val productsForNav =
+                    remember { mutableStateListOf<Product>().apply { addAll(sampleProducts) } }
                 val basket = remember { mutableStateListOf<Product>() }
                 val controller = rememberNavController()
                 NavHost(navController = controller, startDestination = "ProductsScreen") {
                     composable("ProductsScreen") {
                         ProductsScreen(
-                            products = sampleProducts,
+                            products = productsForNav,
                             profileScreen = { controller.navigate("ProfileScreen") },
                             detailScreen = { controller.navigate("DetailScreen/$it") },
                             navigateToProductPage = { controller.navigate("ProductsScreen") },
@@ -127,7 +137,7 @@ class TestNavigationBetweenScreens {
                         })
                     ) { backStackEntry ->
                         val productId = backStackEntry.arguments?.getInt("productId") ?: 0
-                        val product = productsForNav.firstOrNull { it.id == productId }
+                        val product = productsForNav.find { it.id == productId }
                         DetailScreen(
                             product = product,
                             addToBascket = { product?.let { basket.add(it) } },
@@ -154,83 +164,27 @@ class TestNavigationBetweenScreens {
                 }
             }
         }
-        rule.onNodeWithContentDescription("ПрофильКнопка")
-            .performClick()
-        rule.onNodeWithText("Ryqite").assertIsDisplayed()
-        rule.onNodeWithContentDescription("НазадИзПрофиля")
-            .performClick()
-        rule.onNodeWithText("Продукты").assertIsDisplayed()
-        rule.onNodeWithContentDescription("КорзинаКнопка")
-            .performClick()
-        rule.onNodeWithText("Корзина").assertIsDisplayed()
-        rule.onNodeWithContentDescription("НазадИзКорзины")
-            .performClick()
-        rule.onNodeWithText("Продукты").assertIsDisplayed()
-        rule.onNodeWithText("Тестовый продукт")
-            .performClick()
-        rule.onNodeWithText("Описание").assertIsDisplayed()
-        rule.onNodeWithContentDescription("НазадИзДеталей")
-            .performClick()
-        rule.onNodeWithText("Продукты").assertIsDisplayed()
-        rule.onNodeWithContentDescription("КорзинаКнопка")
-            .performClick()
-        rule.onNodeWithContentDescription("ПродуктыКнопка")
-            .performClick()
-        rule.onNodeWithText("Продукты").assertIsDisplayed()
-    }
-    @Test
-    fun testAddAndRemoveFromBasket() {
-        val testingProduct = mutableStateListOf(product)
-        val basket = mutableStateListOf<Product>()
-
-        rule.setContent {
-            Week5Theme {
-                val navController = rememberNavController()
-                NavHost(navController, startDestination = "ProductsScreen") {
-                    composable("ProductsScreen") {
-                        ProductsScreen(
-                            products = testingProduct,
-                            profileScreen = {},
-                            detailScreen = { navController.navigate("DetailScreen/$it") },
-                            navigateToProductPage = {},
-                            navigateToBascketPage = { navController.navigate("BasketScreen") }
-                        )
-                    }
-                    composable("DetailScreen/{productId}") { backStackEntry ->
-                        val productId = backStackEntry.arguments?.getInt("productId") ?: 0
-                        val product = testingProduct.first { it.id == productId }
-                        DetailScreen(
-                            product = product,
-                            addToBascket = { basket.add(product) },
-                            backIcon = { navController.popBackStack() },
-                            navigateToProductPage = {},
-                            navigateToBascketPage = {}
-                        )
-                    }
-                    composable("BasketScreen") {
-                        BasketScreen(
-                            productsForBuy = basket,
-                            deleteFromBasket = { id -> basket.removeAll { it.id == id } },
-                            backIcon = { navController.popBackStack() },
-                            navigateToProductPage = {},
-                            navigateToBascketPage = {}
-                        )
-                    }
-                }
-            }
+        rule.apply {
+            onNodeWithContentDescription("ПрофильКнопка").performClick()
+            onNodeWithText("Ryqite").assertIsDisplayed()
+            onNodeWithContentDescription("НазадИзПрофиля").performClick()
+            onNodeWithText("Продукты").assertIsDisplayed()
+            onNodeWithContentDescription("КорзинаКнопка").performClick()
+            onNodeWithText("Корзина").assertIsDisplayed()
+            onNodeWithContentDescription("НазадИзКорзины").performClick()
+            onNodeWithText("Продукты").assertIsDisplayed()
+            onNodeWithText("Тестовый продукт").performClick()
+            onNodeWithText("Детали").assertIsDisplayed()
+            onNodeWithText("Добавить в корзину").assertIsDisplayed()
+            onNodeWithText("Добавить в корзину").performClick()
+            onNodeWithContentDescription("НазадИзДеталей").performClick()
+            onNodeWithText("Продукты").assertIsDisplayed()
+            onNodeWithContentDescription("КорзинаКнопка").performClick()
+            onNodeWithText("Тестовый продукт").assertIsDisplayed()
+            onAllNodesWithContentDescription("УдалитьКнопка")[0].performClick()
+            onNodeWithText("Корзина пуста").assertIsDisplayed()
+            onNodeWithContentDescription("ПродуктыКнопка").performClick()
+            onNodeWithText("Продукты").assertIsDisplayed()
         }
-
-        rule.onNodeWithText("Тест")
-            .performClick()
-        rule.onNodeWithText("Добавить в корзину")
-            .performClick()
-        rule.onNodeWithContentDescription("КорзинаКнопка")
-            .performClick()
-        rule.onNodeWithText("Тест")
-            .assertIsDisplayed()
-        rule.onNodeWithContentDescription("УдалитьКнопка")
-            .performClick()
-        rule.onNodeWithText("Корзина пуста")
-            .assertIsDisplayed()
     }
 }
