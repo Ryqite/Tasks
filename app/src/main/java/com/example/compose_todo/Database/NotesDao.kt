f021d701d8ba3ed37abb8cda89ff43ab94ad7241
Ryqite <garbaruk.901@gmail.com>
2025-07-05 14:32:11 +0300

    база данных но есть баг

--- app/src/main/java/com/example/compose_todo/Database/NotesDao.kt
+++ app/src/main/java/com/example/compose_todo/Database/NotesDao.kt
@@ -7,14 +7,25 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Интерфейс для работы с базой данных [NotesDatabase]
+ *
+ * фукнция [insertNewNote] принимает заметку в качестве параметра
+ * и вставляет ее в базу данных
+ * фукнция [updateNote] принимает заметку в качестве параметра
+ *  * и обновляет ее в базе данных
+ *  фукнция [deleteNote] принимает заметку в качестве параметра
+ *  * и удаляет ее из базы данных
+ *  фукнция [getAllNotes] получает все заметки из базы данных
+ */
 @Dao
 interface NotesDao {
     @Insert
     suspend fun insertNewNote(note: Notes)
     @Update
-    suspend fun updateNoteById(note: Notes)
+    suspend fun updateNote(note: Notes)
     @Delete
-    suspend fun deleteNoteById(note: Notes)
+    suspend fun deleteNote(note: Notes)
     @Query("SELECT * FROM Notes")
     fun getAllNotes(): Flow<List<Notes>>
 }
--- app/src/main/java/com/example/compose_todo/MainActivity.kt
+++ app/src/main/java/com/example/compose_todo/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,18 +18,20 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.compose_todo.Database.Notes
+import com.example.compose_todo.Database.NotesDatabase
 import com.example.compose_todo.UIcomponents.MainScreen
 import com.example.compose_todo.UIcomponents.NoteUI
 import com.example.compose_todo.ui.theme.ComposetodoTheme
 
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<NotesViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ComposetodoTheme(darkTheme = true) {
-                val notes = remember { mutableStateListOf<Notes>() }
+                val notes by viewModel.getAllNotes().collectAsState(initial = emptyList())
                 val navController = rememberNavController()
                 NavHost(
                     navController = navController, startDestination = "main"
@@ -36,9 +42,8 @@ class MainActivity : ComponentActivity() {
                         }, transitionToCertainNotePage = { noteId ->
                             navController.navigate("notePage/$noteId")
                         }, onCheckedChange = { noteId, isChecked ->
-                            notes.replaceAll { currentNote ->
-                                if (currentNote.id == noteId) currentNote.copy(isDone = isChecked)
-                                else currentNote
+                            notes.find {it.id == noteId}?.let {note->
+                                viewModel.updateNote(note.copy(isDone = isChecked))
                             }
                         })
                     }
@@ -48,7 +53,7 @@ class MainActivity : ComponentActivity() {
                         NoteUI(backToMainPage = { noteContent ->
                             if (noteContent.isNotBlank()) {
                                 val newId = notes.maxOfOrNull { it.id }?.plus(1) ?: 1
-                                notes.add(Notes(id = newId, content = noteContent))
+                               viewModel.insertNote(Notes(id = newId, content = noteContent))
                             }
                             navController.popBackStack()
                         }, deleteNote = {
@@ -63,12 +68,14 @@ class MainActivity : ComponentActivity() {
                         val noteId = backStackEntry.arguments?.getInt("noteId") ?: 0
                         val existingNote = notes.find { it.id == noteId }
                         NoteUI(backToMainPage = { noteContent ->
-                            notes.replaceAll { note ->
-                                if (note.id == noteId) note.copy(content = noteContent) else note
+                            existingNote?.let {
+                                viewModel.updateNote(it.copy(content = noteContent))
                             }
                             navController.popBackStack()
                         }, deleteNote = {
-                            notes.removeAll { it.id == noteId }
+                            existingNote?.let {
+                                viewModel.deleteNote(it)
+                            }
                             navController.popBackStack()
                         }, content = existingNote?.content ?: ""
                         )
