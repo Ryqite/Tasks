de05b26888e7f3e58a6ed8f8e0cd0d49fcfd5346
Ryqite <garbaruk.901@gmail.com>
2025-08-16 17:00:59 +0300

    улчшил внешний вид профиля

--- app/src/main/java/com/example/week12/Data/Entity/Profile.kt
+++ app/src/main/java/com/example/week12/Data/Entity/Profile.kt
@@ -3,7 +3,7 @@ package com.example.week12.Data.Entity
 import com.example.week12.R
 
 data class Profile(
-    val img: Int = R.drawable.upside_down_gray_cat,
+    val img: String = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS0IQhVr9DDJCq61QX28zCoiqDrvezBh5ylw&s",
     val nickName: String = "Ryqite",
     val fullName: String = "Гарбарук Даниил Александрович"
 )
--- app/src/main/java/com/example/week12/Presentation/Models/ProfileData.kt
+++ app/src/main/java/com/example/week12/Presentation/Models/ProfileData.kt
@@ -3,7 +3,7 @@ package com.example.week12.Presentation.Models
 import com.example.week12.R
 
 data class ProfileData(
-    val img: Int = R.drawable.upside_down_gray_cat,
+    val img: String = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS0IQhVr9DDJCq61QX28zCoiqDrvezBh5ylw&s",
     val nickName: String = "Ryqite",
     val fullName: String = "Гарбарук Даниил Александрович"
 )
